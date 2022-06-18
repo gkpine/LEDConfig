54d008c69229a2502fb72ad25bd4a75e9fb45ad7
gkpine <dgagnonk@ryerson.ca>
2022-06-18 17:17:08 -0400

    matrix with row change colour

---
+++ LEDConfig/ButtonMatrix.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LEDConfig
+{
+    public partial class ButtonMatrix : UserControl
+    {
+
+        private int _rows;
+        private int _ledCount;
+
+
+        public int Rows
+        {
+            get
+            {
+                return _rows;
+            }
+            set
+            {
+                _rows = value; 
+                ClearMatrix();
+                BuildMatrix();
+            }
+        }
+        public int LEDCount { 
+        get
+            {
+                return _ledCount;
+            }
+            set
+            {
+                _ledCount = value;
+                ClearMatrix();
+                BuildMatrix();
+            }
+        }
+
+        private int LEDsPerRow;
+
+        public ButtonMatrix()
+        {
+            InitializeComponent();
+            Rows = 1;
+            LEDCount = 10;
+        }
+
+        private void ButtonMatrix_Load(object sender, EventArgs e)
+        {
+            ClearMatrix();
+            BuildMatrix();
+        }
+
+        private void ClearMatrix()
+        {
+            foreach (Button b in this.Controls.OfType<Button>().ToList())
+            {
+                b.Dispose();
+            }
+        }
+
+        private void BuildMatrix()
+        {
+            LEDsPerRow = LEDCount / Rows;
+            int ledCounter = 1;
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int led = 0; led < LEDsPerRow; led++)
+                {
+                    Button btn = new Button();
+                    this.Controls.Add(btn);
+                    btn.Size = new Size(32, 32);
+                    btn.Name = "button" + row + "_" + ledCounter.ToString();
+                    btn.Location = new Point(led * 32, row * 32);
+                    ledCounter++;
+                }
+
+                Button rowBtn = new Button();
+                this.Controls.Add(rowBtn);
+                rowBtn.AutoSize = true;
+                rowBtn.Name = "btnRowColour_" + row;
+                rowBtn.Text = "Change Row Colour";
+                rowBtn.Location = new Point(LEDsPerRow * 32 + 15, row * 32);
+
+            }
+        }
+
+        public void SetColour(Color c, int row)
+        {
+            var rowBtns = this.Controls.OfType<Button>().ToList().Where(b =>
+            
+                b.Name.StartsWith("button") && b.Name.Replace("button", "").Split("_")[0].Trim() == row.ToString()
+            );
+
+            foreach(Button b in rowBtns)
+            {
+                b.BackColor = c;
+            }
+        }
+
+        //public void SetColour(Color c, int buttonIndex)
+        //{
+        //    List<Button> btnList = this.Controls.OfType<Button>().ToList().Where(b => b.Name.StartsWith("button") && b.Name.Replace("button", "").Split("_")[1].Trim() == buttonIndex.ToString()).ToList();
+        //    btnList[0].BackColor = c;
+        //}
+
+        public void SetColour(Color c)
+        {
+            foreach(Button b in this.Controls.OfType<Button>().ToList())
+            {
+                b.BackColor = c;
+            }
+        }
+    }
+}
--- LEDConfig/Form1.Designer.cs
+++ LEDConfig/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonMatrix = new LEDConfig.ButtonMatrix();
+            this.panMatrix = new System.Windows.Forms.Panel();
+            this.panControls = new System.Windows.Forms.Panel();
+            this.panMatrix.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonMatrix
+            // 
+            this.buttonMatrix.AutoSize = true;
+            this.buttonMatrix.LEDCount = 252;
+            this.buttonMatrix.Location = new System.Drawing.Point(0, 0);
+            this.buttonMatrix.Name = "buttonMatrix";
+            this.buttonMatrix.Rows = 12;
+            this.buttonMatrix.Size = new System.Drawing.Size(813, 555);
+            this.buttonMatrix.TabIndex = 0;
+            // 
+            // panMatrix
+            // 
+            this.panMatrix.AutoScroll = true;
+            this.panMatrix.Controls.Add(this.buttonMatrix);
+            this.panMatrix.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panMatrix.Location = new System.Drawing.Point(0, 0);
+            this.panMatrix.Name = "panMatrix";
+            this.panMatrix.Size = new System.Drawing.Size(865, 569);
+            this.panMatrix.TabIndex = 1;
+            // 
+            // panControls
+            // 
+            this.panControls.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panControls.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panControls.Location = new System.Drawing.Point(865, 0);
+            this.panControls.Name = "panControls";
+            this.panControls.Size = new System.Drawing.Size(200, 569);
+            this.panControls.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1065, 569);
+            this.Controls.Add(this.panMatrix);
+            this.Controls.Add(this.panControls);
+            this.Name = "Form1";
+            this.Text = "LEDConfig";
+            this.panMatrix.ResumeLayout(false);
+            this.panMatrix.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ButtonMatrix buttonMatrix;
+        private Panel panMatrix;
+        private Panel panControls;
     }
 }
